03fc18f1e951f3823bd432bb421943f2ebbc6783
WaseemTalib <waseemltalib@gmail.com>
2026-06-16 18:53:39 +0500

    Change button title

--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -45,7 +45,7 @@ export default async function DashboardUsersPage() {
               <UserActionButton
                 id={user.id}
                 action={promoteUserAction}
-                label="Promote to admin"
+                label="Approve to admin"
                 pendingLabel="Promoting…"
                 variant="default"
                 icon={<ShieldCheck size={14} />}
